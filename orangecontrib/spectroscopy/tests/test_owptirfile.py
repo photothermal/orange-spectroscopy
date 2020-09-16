dd31f3154001f4f7492c574638d50eb513f49c9c
AustinTisor <62664115+AustinTisor@users.noreply.github.com>
2020-09-16 13:52:07 -0700

    Add PTIR File widget

---
+++ orangecontrib/spectroscopy/tests/test_owptirfile.py
@@ -0,0 +1,32 @@
+import os.path
+
+import Orange
+from Orange.widgets.tests.base import WidgetTest
+
+from orangecontrib.spectroscopy.widgets.owptirfile import OWPTIRFile
+from orangecontrib.spectroscopy import get_sample_datasets_dir
+
+PHOTOTHERMAL_HYPER = "photothermal/Hyper_Sample.ptir"
+PHOTOTHERMAL_ARRAY = "photothermal/Nodax_Spectral_Array.ptir"
+
+
+class TestOWPTIRFile(WidgetTest):
+
+    def setUp(self):
+        self.widget = self.create_widget(OWPTIRFile)
+
+    def test_load_hyperspectral(self):
+        path = os.path.join(get_sample_datasets_dir(), PHOTOTHERMAL_HYPER)
+        self.widget.add_path(path)
+        self.widget.source = self.widget.LOCAL_FILE
+        self.widget.load_data()
+        self.wait_until_stop_blocking()
+        self.assertNotEqual(self.get_output("Data"), None)
+
+    def test_load_array(self):
+        path = os.path.join(get_sample_datasets_dir(), PHOTOTHERMAL_ARRAY)
+        self.widget.add_path(path)
+        self.widget.source = self.widget.LOCAL_FILE
+        self.widget.load_data()
+        self.wait_until_stop_blocking()
+        self.assertNotEqual(self.get_output("Data"), None)
