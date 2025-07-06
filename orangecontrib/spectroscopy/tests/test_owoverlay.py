1bd20aa0a056adf45aa6a1f6c9ea2c87334e975b
Gergely Nemeth <ngergihun@gmail.com>
2025-07-06 11:33:40 +0200

    OWOverlay basic widget implementation and lint

---
+++ orangecontrib/spectroscopy/tests/test_owoverlay.py
@@ -0,0 +1,72 @@
+import unittest
+from unittest.mock import patch
+import numpy as np
+
+import Orange
+from Orange.widgets.tests.base import WidgetTest
+from orangecontrib.spectroscopy.widgets.owoverlay import OWOverlay
+from orangecontrib.spectroscopy.tests.test_owhyper import wait_for_image
+
+NAN = float("nan")
+
+
+class TestOWOverlay(WidgetTest):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        # cls.iris = Orange.data.Table("iris")
+        cls.whitelight = Orange.data.Table("whitelight.gsf")
+        cls.whitelight_unknown = cls.whitelight.copy()
+        with cls.whitelight_unknown.unlocked():
+            cls.whitelight_unknown[0][0] = NAN
+
+    def setUp(self):
+        self.widget = self.create_widget(OWOverlay)  # type: OWOverlay
+
+    def test_feature_init(self):
+        self.send_signal("Overlay Data", self.whitelight)
+        self.assertEqual(self.widget.attr_value.name, "1.000000")
+
+    def test_context_not_open_invalid(self):
+        self.send_signal("Overlay Data", None)
+        self.assertIsNone(self.widget.imageplot.attr_x)
+        self.send_signal("Overlay Data", self.whitelight)
+        self.assertIsNotNone(self.widget.imageplot.attr_x)
+
+    def test_unknown(self):
+        self.send_signal("Overlay Data", self.whitelight[:10])
+        wait_for_image(self.widget)
+        levels = self.widget.imageplot.img.levels
+        self.send_signal("Overlay Data", self.whitelight_unknown[:10])
+        wait_for_image(self.widget)
+        levelsu = self.widget.imageplot.img.levels
+        np.testing.assert_equal(levelsu, levels)
+
+    def test_set_variable_color(self):
+        data = self.whitelight
+        self.send_signal("Overlay Data", data)
+        with patch(
+            "orangecontrib.spectroscopy.widgets.owhyper.ImageItemNan.setLookupTable"
+        ) as p:
+            self.widget.attr_value = data.domain["1.000000"]
+            self.widget.imageplot.update_color_schema()
+            self.widget._update_feature_value()
+            wait_for_image(self.widget)
+            np.testing.assert_equal(
+                len(p.call_args[0][0]), 256
+            )  # 256 for a continuous variable
+
+    def test_add_visible_image(self):
+        data = self.whitelight
+        self.send_signal("Data", data)
+        self.send_signal("Overlay Data", data)
+        self.widget._attr_changed()
+        self.widget._update_feature_value()
+        wait_for_image(self.widget)
+
+        out = self.get_output("Decorated Data")
+        self.assertIsNotNone(out.attributes["visible_images"])
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ orangecontrib/spectroscopy/widgets/owoverlay.py
@@ -0,0 +1,301 @@
+import numpy as np
+
+from AnyQt.QtCore import Qt, QBuffer
+
+from Orange.data import Table, ContinuousVariable, Domain
+from Orange.widgets.settings import DomainContextHandler, ContextSetting
+from Orange.widgets.utils.itemmodels import DomainModel
+from Orange.widgets.widget import OWWidget, Input, Output, Msg
+from Orange.widgets import gui, settings
+
+from orangewidget.settings import SettingProvider, ContextSetting, Setting
+
+import io
+from orangecontrib.spectroscopy.io.util import ConstantBytesVisibleImage
+from PIL import Image
+from orangecontrib.spectroscopy.utils import get_ndim_hyperspec
+
+from orangecontrib.spectroscopy.widgets.owhyper import BasicImagePlot
+from orangecontrib.spectroscopy.widgets.utils import (
+    SelectionGroupMixin,
+    SelectionOutputsMixin,
+)
+
+
+# Copied from orangecontrib.snom.widgets.owpreprocessimage
+class AImagePlot(BasicImagePlot):
+    attr_x = None  # not settings, set from the parent class
+    attr_y = None
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.axes_settings_box.hide()
+        self.rgb_settings_box.hide()
+
+    def add_selection_actions(self, _):
+        pass
+
+    def clear_markings(self):
+        pass
+
+
+class ImagePreview:
+    imageplot = SettingProvider(AImagePlot)
+
+    value_type = 1
+
+    def __init__(self):
+        self.imageplot = AImagePlot(self)
+
+    def shutdown(self):
+        self.imageplot.shutdown()
+
+
+class OWOverlay(OWWidget):
+    name = "Add Visible"
+    description = "Adds a visible image to the dataset. "
+    icon = "icons/bin.svg"
+
+    settings_version = 2
+    value_type = 1
+
+    settingsHandler = DomainContextHandler()
+    imageplot = SettingProvider(AImagePlot)
+
+    autocommit = settings.Setting(True)
+
+    attr_value = ContextSetting(None)
+    attr_x = ContextSetting(None, exclude_attributes=True)
+    attr_y = ContextSetting(None, exclude_attributes=True)
+
+    # Define inputs and outputs
+    class Inputs:
+        maindata = Input("Data", Table, default=True)
+        data = Input("Overlay Data", Table)
+
+    class Outputs:
+        outdata = Output("Decorated Data", Table, default=True)
+
+    class Error(OWWidget.Error):
+        invalid_axis = Msg("Invalid axis: {}")
+        invalid_block = Msg("Bin block size not compatible with dataset: {}")
+        image_too_big = Msg("Image for chosen features is too big ({} x {}).")
+
+    class Warning(OWWidget.Warning):
+        nan_in_image = Msg("Unknown values within images: {} unknowns")
+        threshold_error = Msg("Low slider should be less than High")
+
+    class Information(SelectionOutputsMixin.Information):
+        not_shown = Msg("Undefined positions: {} data point(s) are not shown.")
+
+    def image_values(self):
+        attr_value = self.attr_value.name if self.attr_value else None
+        return lambda data, attr=attr_value: data.transform(Domain([data.domain[attr]]))
+
+    def image_values_fixed_levels(self):
+        return None
+
+    def __init__(self):
+        super().__init__()
+
+        self.maindata = None
+        self.data = None
+
+        # Imageplot for mainarea
+        self.imageplot = AImagePlot(self)
+        self.mainArea.layout().addWidget(self.imageplot)
+
+        # Control area box
+        self.imagebox = gui.widgetBox(self.controlArea, "Image")
+
+        self.xy_model = DomainModel(
+            DomainModel.METAS | DomainModel.CLASSES, valid_types=ContinuousVariable
+        )
+
+        self.feature_value_model = DomainModel(
+            order=(
+                DomainModel.ATTRIBUTES,
+                DomainModel.Separator,
+                DomainModel.CLASSES,
+                DomainModel.Separator,
+                DomainModel.METAS,
+            ),
+            valid_types=ContinuousVariable,
+        )
+
+        self.contextAboutToBeOpened.connect(lambda x: self._init_interface_data(x[0]))
+
+        common_options = dict(
+            labelWidth=50, orientation=Qt.Horizontal, sendSelectedValue=True
+        )
+
+        box = gui.vBox(self.imagebox)
+
+        self.feature_value = gui.comboBox(
+            box,
+            self,
+            "attr_value",
+            label="Feature:",
+            contentsLength=12,
+            searchable=True,
+            callback=self._update_feature_value,
+            model=self.feature_value_model,
+            **common_options,
+        )
+
+        gui.comboBox(
+            box,
+            self,
+            f"attr_x",
+            label=f"Axis X:",
+            callback=self._attr_changed,
+            model=self.xy_model,
+            **common_options,
+        )
+        gui.comboBox(
+            box,
+            self,
+            f"attr_y",
+            label=f"Axis Y:",
+            callback=self._attr_changed,
+            model=self.xy_model,
+            **common_options,
+        )
+
+        colorsbox = gui.widgetBox(self.controlArea, "Image colors")
+        colorsbox.layout().addWidget(self.imageplot.color_settings_box)
+
+        gui.rubber(self.controlArea)
+        gui.auto_commit(self.controlArea, self, "autocommit", "Send Data")
+
+        self.imageplot.color_cb.activated.connect(self.commit.deferred)
+        self.imageplot._threshold_low_slider.sliderReleased.connect(
+            self.commit.deferred
+        )
+        self.imageplot._threshold_high_slider.sliderReleased.connect(
+            self.commit.deferred
+        )
+        self.imageplot._level_high_le.editingFinished.connect(self.commit.deferred)
+        self.imageplot._level_low_le.editingFinished.connect(self.commit.deferred)
+        self._init_attr_values(self.data)
+
+    def _attr_changed(self):
+        self._update_attrs()
+        self.commit.deferred()
+
+    def _init_attr_values(self, data):
+        domain = data.domain if data is not None else None
+        self.feature_value_model.set_domain(domain)
+        self.attr_value = (
+            self.feature_value_model[0] if self.feature_value_model else None
+        )
+        self.xy_model.set_domain(domain)
+        self.attr_x = self.xy_model[0] if self.xy_model else None
+        self.attr_y = self.xy_model[1] if len(self.xy_model) >= 2 else self.attr_x
+
+    def _init_interface_data(self, data):
+        self._init_attr_values(data)
+        self.imageplot.init_interface_data(data)
+
+    def _update_feature_value(self):
+        self.redraw_data()
+
+    def _update_attrs(self):
+        self.imageplot.attr_x = self.attr_x
+        self.imageplot.attr_y = self.attr_y
+        self.redraw_data()
+
+    def redraw_data(self):
+        self.imageplot.update_view()
+        self.commit.deferred()
+
+    def set_vimage(self, data):
+        # Extract the image scales
+        if data is not None:
+            try:
+                hypercube, ls = get_ndim_hyperspec(
+                    data, (self.imageplot.attr_x, self.imageplot.attr_y)
+                )
+            except Exception as e:
+                self.Error.invalid_axis(str(e))
+                return
+
+            width = np.abs(ls[0][1] - ls[0][0])
+            height = np.abs(ls[1][1] - ls[1][0])
+            xres = width / hypercube.shape[:2][0]
+            yres = height / hypercube.shape[:2][1]
+            posx = ls[0][0]
+            posy = ls[1][0]
+
+            # Extract the image from imageplot so the coloring is done
+            im = self.imageplot.img.qimage
+            if im is not None:
+                buffer = QBuffer()
+                buffer.open(QBuffer.ReadWrite)
+                im.save(buffer, "PNG")
+                pil_im = Image.open(io.BytesIO(buffer.data()))
+                pil_im = pil_im.transpose(Image.FLIP_TOP_BOTTOM)
+                img_bytes = io.BytesIO()
+                pil_im.save(img_bytes, format="PNG")
+
+                # Need to modify the position and the scale since visual imageplot
+                # place the corner of the pixel to the given position
+                vimage = ConstantBytesVisibleImage(
+                    name="External Image",
+                    pos_x=posx - xres / 2,
+                    pos_y=posy - yres / 2,
+                    size_x=width + xres,
+                    size_y=height + yres,
+                    image_bytes=img_bytes,
+                )
+
+                # Assign it to the datatable attributes
+                if self.maindata and vimage is not None:
+                    self.maindata.attributes["visible_images"] = [vimage]
+
+    @Inputs.maindata
+    def set_data(self, data):
+        if data is not None:
+            self.maindata = data
+        else:
+            self.maindata = None
+
+        self.commit.deferred()
+
+    @Inputs.data
+    def set_overlaydata(self, data):
+        self.closeContext()
+        self.openContext(data)
+        if data is not None:
+            self.data = data
+        else:
+            self.data = None
+
+        self.Warning.nan_in_image.clear()
+        self.Error.invalid_axis.clear()
+        self.Error.invalid_block.clear()
+
+        self.imageplot.set_data(data)
+        self.imageplot.update_view()
+
+        self.commit.deferred()
+
+    @gui.deferred
+    def commit(self):
+        self.Warning.nan_in_image.clear()
+        self.Error.invalid_axis.clear()
+        self.Error.invalid_block.clear()
+
+        self.set_vimage(self.data)
+
+        self.Outputs.outdata.send(self.maindata)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    from Orange.widgets.utils.widgetpreview import WidgetPreview
+    import Orange.data
+
+    WidgetPreview(OWOverlay).run(
+        set_data=Table("agilent/5_mosaic_agg1024.dmt"),
+        set_overlaydata=Table("agilent/5_mosaic_agg1024.dmt"),
+    )
