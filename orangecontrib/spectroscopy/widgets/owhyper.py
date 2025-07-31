d3f4dd82a9649725739d4d269e0efa8bfeb11468
Stuart Read <stuart.read@lightsource.ca>
2025-07-31 15:35:00 -0600

    [FIX] owhyper: Show RGB limits settings in color box when selected.

--- orangecontrib/spectroscopy/widgets/owhyper.py
+++ orangecontrib/spectroscopy/widgets/owhyper.py
@@ -1775,6 +1775,7 @@ class OWHyper(OWWidget, SelectionOutputsMixin):
         # add image settings to the main pane after ImagePlot.__init__
         iabox.layout().addWidget(self.imageplot.axes_settings_box)
         icbox.layout().addWidget(self.imageplot.color_settings_box)
+        icbox.layout().addWidget(self.imageplot.rgb_settings_box)
         ivbox.layout().addWidget(self.imageplot.setup_vector_plot_controls())
 
         self.data = None
